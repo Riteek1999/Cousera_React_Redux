744d35b5e2fe747c85171ad3962b2e11deb5fbd1
Riteek <riteek2000@gmail.com>
2020-07-18 14:56:01 +0530

    Fetch and Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,12 +4,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CommentFormComponent from './CommentFormComponent';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
     function RenderDish({dish, isLoading, errMess}) {
         if (dish != null)
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                   <CardTitle>{dish.name}</CardTitle>
                   <CardText>{dish.description}</CardText>
